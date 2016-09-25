b1190969d00a0ff5068283fd069777a3d303e8f0
sonikaarora <sonikaarora@Sonikas-MacBook-Pro.local>
2016-09-25 11:33:48 -0700

    Final Code

---
+++ ServerEnd/Kendal/routes/careTakerOperations.js
@@ -0,0 +1,65 @@
+/**
+ * New node file
+ */
+var fs = require('fs');
+var PDFParser = require("pdf2json");
+var mysql = require("./mysql.js");
+var pdfMake = require("pdfmake");
+
+function careTakerShowDocument(req,res){
+	
+		console.log(req.body.documentName);
+		res.header("Access-Control-Allow-Origin", "*");
+		var pdfParser = new PDFParser(this,1);
+		pdfParser.on("pdfParser_dataError", function(errData){
+			console.error(errData.parserError);
+		});
+		pdfParser.on("pdfParser_dataReady", function(pdfData){
+			fs.writeFile("./content.txt", pdfParser.getRawTextContent());
+		});
+		pdfParser.loadPDF("./common/"+req.body.documentName);
+		res.sendfile('content.txt');
+	
+}
+
+
+function careTakerHomePage(req,res){
+	
+	res.header("Access-Control-Allow-Origin", "*");
+	var sqlquery="select * from documentpending where caretaker_email=";
+	sqlquery+= "'"+req.body.email+"'";
+	sqlquery+= ";";
+		
+	mysql.fetchData(sqlquery,function(err,rows){
+		if(rows.length > 0){
+			var result={"status":"200","rows":rows};
+			res.send(result);
+		}else{
+			var result={"status":"400"};
+			res.send(result);
+		}
+	});
+	
+	
+}
+
+
+function nitesh(req,res){
+	console.log("nitesh wadhwa");
+	res.header("Access-Control-Allow-Origin", "*");
+	var pdf = req.body.dataImg;
+	console.log(pdf);
+	pdf = pdf.replace('data:image/png;base64,', '');
+	console.log(pdf);
+	fs.writeFile("xyz.pdf",pdf, 'base64', function(err) { 
+						console.log(err); 
+			}); 
+}
+	
+
+
+
+
+exports.nitesh=nitesh;
+exports.careTakerHomePage=careTakerHomePage;
+exports.careTakerShowDocument=careTakerShowDocument;
